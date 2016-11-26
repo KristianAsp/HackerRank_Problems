f0f85872cb1a8eac7e6a6e8160dc416c535a702f
Kristian André Aspevik <kristian.aspevik@gmail.com>
2016-11-26 21:39:24 +0000

    Added files for already completed challenges/tasks. Started the 30 Days of Coding challenge as well as SQL tasks

---
+++ src/Challenges/Thirty_Days_Of_Coding/Day_One.java
@@ -0,0 +1,19 @@
+package Challenges.Thirty_Days_Of_Coding;
+
+import java.util.Scanner;
+
+/**
+ * Created by Kristian on 26/11/2016.
+ */
+public class Day_One {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        String inputString = scan.nextLine();
+
+        scan.close();
+
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+
+    }
+}
