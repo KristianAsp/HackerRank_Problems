10984c9ce176bd77878df9b973a226c39467535d
Kristian André Aspevik <kristian.aspevik@gmail.com>
2016-11-30 13:14:44 +0000

    Day 3 of 30 Days of Coding Challenge. Straight forward use of the Modulus operator

---
+++ src/Challenges/Thirty_Days_Of_Coding/Day_3.java
@@ -0,0 +1,35 @@
+package Challenges.Thirty_Days_Of_Coding;
+
+import java.util.Scanner;
+
+/**
+ * Created by Kristian André Aspevik on 30/11/2016.
+ * https://www.hackerrank.com/challenges/30-conditional-statements
+ */
+public class Day_3 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        String result = "";
+
+        // If the remainder of n/2 = 1, it is odd
+        if(n % 2 == 1){
+            result = "Weird";
+        }
+        else{
+            if(n > 1 && n < 6) {
+                result = "Not Weird";
+            }
+            else if(n > 5 && n < 21){
+                result = "Weird";
+            }
+            else if(n > 20){
+                result = "Not Weird";
+            }
+        }
+        System.out.println(result);
+    }
+
+}
